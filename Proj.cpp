9c7bccda1c01e4df2a7743c45c236f5de73df63c
jlucas <117687632+l28j@users.noreply.github.com>
2023-11-16 01:20:16 +0000

    Commit
    miau

---
+++ Proj.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+struct Board {
+    int a, b, p;
+    float pricePerArea;
+    Board(int a, int b, int p) : a(a), b(b), p(p) {
+        pricePerArea = static_cast<double>(p) / (a * b);
+    }
+};
+
+void addToDb(std::vector<Board>& Db, int a, int b, int p) {
+    Board newBoard(a, b, p);
+    Db.push_back(newBoard);
+}
+
+bool compareByPricePerArea(const Board& a, const Board& b) {
+    return a.pricePerArea > b.pricePerArea;
+}
+
+void sortDb(std::vector<Board>& Db) {
+    std::sort(Db.begin(), Db.end(), compareByPricePerArea);
+}
+
+int calculateOutput(int X, int Y, int Db) { return 0; }
+
+void showOutput(std::vector<Board>& Db, int X, int Y, int n) {
+
+    for (const Board& board : Db) {
+        std::cout << "Db:";
+        std::cout << board.a << " " << board.b << " " << board.p << " " << board.pricePerArea << std::endl;
+    }
+
+    std::cout << "X: " << X << std::endl;
+    std::cout << "Y: " << Y << std::endl;
+    std::cout << "N: " << n << std::endl;
+}
+
+int main() {
+
+    std::string userInput;
+    int X, Y, n;
+
+    std::getline(std::cin, userInput);
+    std::istringstream iss(userInput);
+    iss >> X >> Y;
+
+    std::getline(std::cin, userInput);
+    std::istringstream iss2(userInput);
+    iss2 >> n;
+
+    std::vector<Board> Db;
+
+    for (int i = 0; i < n; i++) {
+        std::getline(std::cin, userInput);
+        std::istringstream iss3(userInput);
+        int a, b, p;
+        iss3 >> a >> b >> p;
+
+        if ((a <= X && b <= Y) || (a <= Y && b <= X)) { addToDb(Db, a, b, p); }
+    }
+
+    sortDb(Db);
+
+    //int maxPrice = calculateOutput(X, Y, Db);
+    //std::cout << maxPrice << std::endl;
+    showOutput(Db, X, Y, n);
+
+    return 0;
+}
