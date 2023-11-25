4fc7ec65dd9719e117ac90d8f40e931b86cfbf44
Mariana Santana <mariana.marcal.santana@tecnico.ulisboa.pt>
2023-11-25 21:26:32 +0000

    proj 1600

--- Proj.cpp
+++ Proj.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <map>
 
 struct Slate {
     int a, b, p;
@@ -27,10 +28,10 @@ int result(const std::vector<Slate>& slates, int X, int Y) {
         for (int j = 0 ; j <= Y ; j ++) {
             for (int k = 0 ; k < numElements ; k ++) {
                 
-                    Slate slate = slates[k];    
-                    int a = slate.a;
-                    int b = slate.b;
-                    int p = slate.p;
+                Slate slate = slates[k];    
+                int a = slate.a;
+                int b = slate.b;
+                int p = slate.p;
 
                 if ((i==a && j ==b) || (i==b && j==a)){
                     values[i][j] = max_value(values[i][j], p);
@@ -58,22 +59,85 @@ int result(const std::vector<Slate>& slates, int X, int Y) {
     return values[X][Y];
 }
 
-int result2(const std::vector<std::vector<int>> m, int X, int Y) {
+int result2(std::vector<std::vector<int>>& m, int X, int Y) {
 
     for (int i = 0; i < X; i++) {
         for (int j = 0; j < Y; j++) {
             
-            //cut horizontal
+            for (int k = 0; k < i; k++) {
+                m[i][j] = max_value(m[i][j], m[k][j] + m[i - k - 1][j]);
+            }
+            for (int k = 0; k < j; k++) {
+                m[i][j] = max_value(m[i][j], m[i][k] + m[i][j - k - 1]);
+            }
 
+
+
+            //if (i == 0 && j == 0) { continue; }
+            //else if (i == 0 && j > 0) { m[i][j] = max_value(m[i][j], m[i][j - 1] + m[i][Y - j]); }
+            //else if (j == 0 && i > 0) { m[i][j] = max_value(m[i][j], m[i - 1][j] + m[X - i][j]); }
+
+            //else {
+                //m[i][j] = max_value(m[i][j], m[i - 1][j] + m[X - i][j]);
+                //m[i][j] = max_value(m[i][j], m[i][j - 1] + m[i][Y - j]);
+            //}
+            //cut horizontal
+            //if (i == 0 && j > 0) { m[i][j] = max_value(m[i][j], m[i][j - 1] + m[i][Y - j]); }
+            //if (i == 0 && j > 0) { m[i][j] = max_value(m[i][j], result2(m,i,j - 1)); }
+            //else if (i > 0 && j > 0) { m[i][j] = max_value(m[i][j], m[i - 1][j]); }
 
             //cut vertical
+            //if (j == 0 && i > 0) { m[i][j] = max_value(m[i][j], m[i - 1][j]); }
+            //else if (j > 0 && i > 0) { m[i][j] = max_value(m[i][j], m[i][j - 1]); }
 
             //fits slate
             
         }
     }
+    //if (X == 0 || Y == 0) { return m[X][Y]; }
+    return m[X - 1][Y - 1];
 }
-    
+
+int result3(const std::map<std::string, Slate>& slatesMap, int X, int Y) {
+
+    std::vector<std::vector<int>> values(X+1, std::vector<int>(Y+1, 0));   
+
+    for (int i = 0 ; i <= X ; i ++) {
+        for (int j = 0 ; j <= Y ; j ++) {
+            
+            std::stringstream ss;
+            ss << i << "-" << j;
+            std::string key = ss.str();
+
+            if (slatesMap.find(key) != slatesMap.end()) {
+                Slate slate = slatesMap.at(key);
+                int p = slate.p;
+                values[i][j] = max_value(values[i][j], p);
+            }
+
+            std::stringstream ss2;
+            ss2 << j << "-" << i;
+            std::string key2 = ss2.str();
+
+            if (slatesMap.find(key2) != slatesMap.end()) {
+                Slate slate = slatesMap.at(key2);
+                int p = slate.p;
+                values[i][j] = max_value(values[i][j], p);
+            }
+            else {
+                for (int k = 0 ; k < i ; k ++) {
+                    values[i][j] = max_value(values[i][j], values[k][j] + values[i-k][j]);
+                }
+                for (int k = 0 ; k < j ; k ++) {
+                    values[i][j] = max_value(values[i][j], values[i][k] + values[i][j-k]);
+                }
+            }
+        }
+    }
+
+    return values[X][Y];
+}
+
 int main() {
 
     std::string userInput;
@@ -88,7 +152,7 @@ int main() {
     iss2 >> n;
 
     std::vector<Slate> slates;
-    std::vector<std::vector<int>> m(X , std::vector<int>(Y, 0));
+    std::vector<std::vector<int>> m(X, std::vector<int>(Y, 0));
 
     for (int i = 0; i < n; i++) {
         std::getline(std::cin, userInput);
@@ -100,6 +164,7 @@ int main() {
         if (fits(a, b, X, Y) && p > 0) {
             slates.emplace_back(a, b, p);
         }
+
         if (allFits(a, b, X, Y) && p > 0) {
             m[a-1][b-1] = max_value(m[a-1][b-1], p);
             m[b-1][a-1] = max_value(m[b-1][a-1], p);
@@ -120,8 +185,10 @@ int main() {
         std::cout << std::endl; // Move to the next line after printing a row
     }*/
 
-    int maxPrice = result(slates, X, Y);
-    std::cout << maxPrice << std::endl;
+    //int maxPrice = result(slates, X, Y);
+    int maxPrice2 = result2(m, X, Y);
+
+    std::cout << maxPrice2 << std::endl;
 
     return 0;
 }
